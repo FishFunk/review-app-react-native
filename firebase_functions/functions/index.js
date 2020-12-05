dbbd3aad297f37b684fe2caf8363f9f319436cc9
Daniel Fishman <fishfry62@gmail.com>
2020-12-04 17:55:20 -0800

    updates to functions

--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -30,8 +30,8 @@ exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 19 * * 0').onRun
         payloads.push({
           // Notification details payload
           to: user.push_tokens,
-          title: `Epic weekend ${user.firstName}?`,
-          body: `Drop a review if you went to any place new!`
+          title: `Fun weekend ${user.firstName}?`,
+          body: `Drop a review if you tried any place new!`
         });
       }
     });
@@ -39,74 +39,79 @@ exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 19 * * 0').onRun
     return _sendExpoTokenRequest(payloads);
 });
 
+
+
+
+
+
 /**
  * Triggers when a user  writes a review
  * */
-exports.sendReviewNotification = functions.database.ref('/reviews/{place_id}')
-    .onWrite(async (change, context) => {
-      // ignore delete
-      if(context.eventId === 'google.firebase.database.ref.delete' || 
-        context.eventId === 'google.firestore.document.delete')
-      {
-        return;
-      }
+// exports.sendReviewNotification = functions.database.ref('/places/{place_id}/reviews/{review_id}')
+//     .onWrite(async (change, context) => {
+//       // ignore delete
+//       if(context.eventId === 'google.firebase.database.ref.delete' || 
+//         context.eventId === 'google.firestore.document.delete')
+//       {
+//         return;
+//       }
       
-      const place_id = context.params.place_id;
+//       const place_id = context.params.place_id;
       
-      const review_list_before_change = change.before.val() || [];
-      const review_list_after_change = change.after.val() || [];
+//       const review_list_before_change = change.before.val() || [];
+//       const review_list_after_change = change.after.val() || [];
 
-      const newReview = _.first(_.difference(review_list_after_change, review_list_before_change));
-      // If un-follow we exit the function.
-      if (!newReview) {
-        return;
-      }
+//       const newReview = _.first(_.difference(review_list_after_change, review_list_before_change));
+//       // If un-follow we exit the function.
+//       if (!newReview) {
+//         return;
+//       }
 
-      console.log(`New review for ${newReview.place_name} published by ${newReview.reviewer_id}`);
+//       console.log(`New review for ${newReview.place_name} published by ${newReview.reviewer_id}`);
 
-      const reviewer = await admin.auth().getUser(newReview.reviewer_id);
+//       const reviewer = await admin.auth().getUser(newReview.reviewer_id);
 
-      // Get the list of users to notify
-      let payloads = [];
-      const usersSnapshot = await admin.database().ref(`/users`).once('value');
-      usersSnapshot.forEach((snap)=>{
-        const user = snap.val();
-        if(_.indexOf(user.following, newReview.reviewer_id) >= 0){
-          if(user.push_tokens){
-            payloads.push({
-              // Notification details payload
-              to: user.push_tokens,
-              title: 'Someone you follow wrote a review!',
-              body: `${reviewer.displayName} went to ${newReview.place_name} and gave it ${newReview.avg_rating} stars.`
-            });
-          }
-        }
-      });
+//       // Get the list of users to notify
+//       let payloads = [];
+//       const usersSnapshot = await admin.database().ref(`/users`).once('value');
+//       usersSnapshot.forEach((snap)=>{
+//         const user = snap.val();
+//         if(_.indexOf(user.following, newReview.reviewer_id) >= 0){
+//           if(user.push_tokens){
+//             payloads.push({
+//               // Notification details payload
+//               to: user.push_tokens,
+//               title: 'Someone you follow wrote a review!',
+//               body: `${reviewer.displayName} went to ${newReview.place_name} and gave it ${newReview.avg_rating} stars.`
+//             });
+//           }
+//         }
+//       });
 
-      // Check if there are any device tokens.
-      if (!payloads || payloads.length === 0) {
-        console.log('There are no notifications to be sent.');
-        return;
-      }
+//       // Check if there are any device tokens.
+//       if (!payloads || payloads.length === 0) {
+//         console.log('There are no notifications to be sent.');
+//         return;
+//       }
 
-      console.log(`There are ${tokens.length} tokens to send notifications to.`);
+//       console.log(`There are ${tokens.length} tokens to send notifications to.`);
 
-      // Send notifications to all tokens.
-      try{
-        const response = await _sendExpoTokenRequest(payloads);
+//       // Send notifications to all tokens.
+//       try{
+//         const response = await _sendExpoTokenRequest(payloads);
 
-        if(response.errors){
-          console.error(response.errors);
-        }
+//         if(response.errors){
+//           console.error(response.errors);
+//         }
 
-        if(response.data){
-          response.data.forEach(item =>{
-            if(item.status === 'error'){
-              console.error('error: ' + item.message);
-            }
-          })
-        }
-      } catch (ex){
-        console.error(ex);
-      }
-});
+//         if(response.data){
+//           response.data.forEach(item =>{
+//             if(item.status === 'error'){
+//               console.error('error: ' + item.message);
+//             }
+//           })
+//         }
+//       } catch (ex){
+//         console.error(ex);
+//       }
+// });
