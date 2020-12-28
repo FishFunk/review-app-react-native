3c06984240feefb4df3110a3fd4a7d97e9e079b0
Daniel Fishman <fishfry62@gmail.com>
2020-12-28 14:59:41 -0800

    added app store url

--- constants/Urls.ts
+++ constants/Urls.ts
@@ -1,3 +1,3 @@
 //TODO: Fill in with actual URLs when published or store in Firebase
-export const iosAppStoreUrl = 'https://iOS_APP_STORE_URL';
+export const iosAppStoreUrl = 'https://apps.apple.com/us/app/nobull-reviews/id1531396052#?platform=iphone';
 export const androidPlayStoreUrl = 'https://GOOGLE_PLAY_STORE_URL';
